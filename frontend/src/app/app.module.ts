d68a27a1960489b8c3ca7c62d6a336dfe397baab
Wesley Costa <wesferrcosta@gmail.com>
2021-05-12 22:01:26 -0300

    Adicionado botão criar produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/products/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { ProductCreateComponent } from './components/products/product-create/pro
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
