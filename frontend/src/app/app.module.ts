3625415aa153c71e3f5880ddac4e70b887e10290
Wesley Costa <wesferrcosta@gmail.com>
2021-05-19 20:12:02 -0300

    feat: Consulta ao banco de dados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MatInputModule } from '@angular/material/input';
     RedDirective,
     ForDirective,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
